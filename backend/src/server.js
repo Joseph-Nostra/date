61fce743507d5b9ae354058c30376fd6c10a03a5
Joseph-Nostra <youssefzh850@gmail.com>
2026-08-24 02:32:37 +0100

    ajouter le lien de frontend déployé

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+    'https://date-frontend-black.vercel.app',
 
     // URL ديال React deployed
     'https://date-frontend-black.vercel.app'
